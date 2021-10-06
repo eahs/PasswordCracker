550d586b76f449984d4aed03fed7a54df022c1e8
Michael Tanczos <mike@logic-gate.com>
2021-10-05 21:24:03 -0400

    Removed some bad passwords, added md5 method

--- PasswordCracker/PasswordCracker/Program.cs
+++ PasswordCracker/PasswordCracker/Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace PasswordCracker
 {
     class Program
     {
+        public static string md5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+        
         static void Main(string[] args)
         {
             string[] hashedPasswords = File.ReadAllLines("passwords_hashed.txt");
